45d3f6b724644f8c6cd62ac2aada83eba9ea85f5
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-03-18 14:54:39 +0200

    refactor web routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PostController::class,"index"])->name("posts.index");
-Route::get('/post/{post}', [PostController::class,"show"])->name("posts.show");
 Route::get('/posts/create', [PostController::class,"create"])->name("posts.create");
+Route::get('/posts/{post}', [PostController::class,"show"])->name("posts.show");
 Route::post('/posts', [PostController::class,"store"])->name("posts.store");
