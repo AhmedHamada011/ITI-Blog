f8ea9a5bf7e9f9a4928d55fc1178be352b364f5f
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-03-18 13:41:30 +0200

    add index

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = [
+            ["id"=>1,"title"=>"laravel","posted_by"=>"ahmed","created_at"=>"2023-04-05"],
+
+            ["id"=>2,"title"=>"php","posted_by"=>"mohammed","created_at"=>"2023-04-07"],
+
+            ["id"=>3,"title"=>"javascript","posted_by"=>"ali","created_at"=>"2023-04-08"],
+
+        ];
+
+        return view("posts.index",["posts"=>$posts]);
+    }
+
+
+    public function show()
+    {
+
+    }
+}
